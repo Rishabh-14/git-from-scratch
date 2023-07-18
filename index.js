c2346225da86566ea8666aaa277b4cc72a53bfb7
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-07-18 13:17:54 +0530

    commit and history

--- index.js
+++ index.js
@@ -16,8 +16,22 @@ class Repository {
     this.staging[filename] = this.files[filename];
     console.log(`${filename} is staged`);
   }
+
+  commit(message) {
+    const snapshot = { ...this.staging };
+    this.commits.push({ message, snapshot });
+    this.staging = {};
+  }
+
+  getHistory() {
+    return this.commits.map(
+      (commit, index) => `Commit ${index}: ${commit.message}`
+    );
+  }
 }
 
 const repo = new Repository();
 repo.addFile("file1", "Hello World");
 repo.stageFile("file1");
+repo.commit("Initial commit");
+console.log(repo.getHistory());
