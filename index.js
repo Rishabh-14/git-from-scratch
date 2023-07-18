2b6719e5cfad44219cb06b414fcd8ce680f96750
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-07-18 12:15:16 +0530

    stage file functionality

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ class Repository {
     this.staging = {};
     this.commits = [];
   }
-  addFile(fileName, contents) {
-    this.files[fileName] = contents;
-    console.log(`files name: ${fileName} and content: ${contents}`);
+  addFile(filename, contents) {
+    this.files[filename] = contents;
+    console.log(`files name: ${filename} and content: ${contents}`);
+  }
+
+  stageFile(filename) {
+    if (!this.files[filename]) {
+      throw new Error(`File ${filename} does not exist`);
+    }
+    this.staging[filename] = this.files[filename];
+    console.log(`${filename} is staged`);
   }
 }
 
 const repo = new Repository();
 repo.addFile("file1", "Hello World");
+repo.stageFile("file1");
